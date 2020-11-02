77951a14086b946ccd0288153f47adc3f361fc05
Brittani Baker <brittanibaker07@gmail.com>
2020-11-02 00:50:39 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -52,8 +52,7 @@ def add_characters
          ]
       }
    }
-epic_tragedy[:additional_characters]=[prince = { name: "Prince Escalus" },
-apothecary = { name: "Apothecary" }]
+
 
 epic_tragedy[:additional_characters][0] << { name: "Prince Escalus" }
 epic_tragedy[:additional_characters][1] << { name: "Apothecary" }
